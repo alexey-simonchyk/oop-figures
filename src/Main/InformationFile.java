48dab707c534630a5eb880306f79618274d5df29
Alexey Simonchyk <alexey.simonchyk@gmail.com>
2016-04-12 23:08:34 +0300

    Serializtion

---
+++ src/Main/InformationFile.java
@@ -0,0 +1,49 @@
+package Main;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class InformationFile {
+
+    private File tempFile;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+
+    public void checkExsistFile() {
+        tempFile = new File("temp.bin");
+        try {
+            if (!tempFile.exists())
+                tempFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void write(FigureList list) {
+        try {
+            ArrayList<Figure> temp = list.getList();
+            objectOutputStream = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(tempFile.getAbsoluteFile())));
+                objectOutputStream.writeObject(temp);
+            objectOutputStream.close();
+        }
+        catch (IOException e) {
+            System.out.println(e);
+
+        }
+    }
+
+    public void read(FigureList list) {
+        try {
+            objectInputStream = new ObjectInputStream(new BufferedInputStream(new FileInputStream(tempFile.getAbsoluteFile())));
+
+            list.setList((ArrayList<Figure>) objectInputStream.readObject());
+
+            objectInputStream.close();
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        }
+
+    }
+}
